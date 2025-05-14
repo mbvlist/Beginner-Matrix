69d4a54d7722f2bab3d9002e7a4ae9a92dc6911d
Martin van der Vlist <martin.vandervlist@alten.nl>
2025-05-14 12:15:17 +0200

    Use template<class T> for Matrix
    Also: test const reference

--- Matrix.h
+++ Matrix.h
@@ -3,35 +3,152 @@
 
 #pragma once
 
-#include <iosfwd>
+#include <iomanip>
+#include <iostream>
+#include <utility>
 
+template<class T>
 class Matrix
 {
 public:
-    Matrix(size_t rows, size_t columns);
-    Matrix(const Matrix& m) noexcept;
-    Matrix(Matrix&& m) noexcept;
-    ~Matrix();
+    Matrix(size_t rows, size_t columns) 
+      : m_data(new T[rows * columns]),
+        m_rows(rows),
+        m_columns(columns)
+    {
+        for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+        {
+            m_data[pos] = 0;
+        }
+    }
 
-    Matrix& operator=(const Matrix& m);
-    Matrix& operator=(Matrix&& m) noexcept;
+    Matrix(const Matrix& m) noexcept
+      : m_data(new double[m.m_rows * m.m_columns]),
+        m_rows(m.m_rows),
+        m_columns(m.m_columns)
+    {
+        for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+        {
+            m_data[pos] = m.m_data[pos];
+        }
+    }
 
-    Matrix operator+(const Matrix& m) const;
-    Matrix operator*(double d) const;
+    Matrix(Matrix&& m) noexcept
+      : m_data(m.m_data),
+        m_rows(m.m_rows),
+        m_columns(m.m_columns)
+    {
+        m.m_data = nullptr;
+        m.m_rows = 0;
+        m.m_columns = 0;
+    }
 
-    friend Matrix operator*(double d, const Matrix& m);
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+    ~Matrix()
+    {
+        delete[] m_data;
+    }
 
-    double& operator()(size_t row, size_t col) { return m_data[pos(row, col)]; }
-    const double& operator()(size_t row, size_t col) const { return m_data[pos(row, col)]; }
+    Matrix& operator=(const Matrix& m)
+    {
+        if (this != &m)
+        {
+            delete[] m_data;
 
-    double max() const;
+            m_rows = m.m_rows;
+            m_columns = m.m_columns;
+
+            m_data = new T[m_rows * m_columns];
+
+            for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+            {
+                m_data[pos] = m.m_data[pos];
+            }
+        }
+
+        return *this;
+    }
+    
+    Matrix& operator=(Matrix&& m) noexcept
+    {
+        std::swap(m_data, m.m_data);
+        std::swap(m_rows, m.m_rows);
+        std::swap(m_columns, m.m_columns);
+        return *this;
+    }
+
+    Matrix operator+(const Matrix& m) const
+    {
+        if (m_rows != m.m_rows || m_columns != m.m_columns)
+        {
+            throw std::exception(); // TODO: define exception type
+        }
+
+        Matrix result(m_rows, m_columns);
+
+        for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+        {
+            result.m_data[pos] = m_data[pos] + m.m_data[pos];
+        }
+
+        return result;
+    }
+
+    Matrix operator*(const T& d) const
+    {
+        Matrix result(m_rows, m_columns);
+
+        for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+        {
+            result.m_data[pos] = m_data[pos] * d;
+        }
+
+        return result;
+    }
+
+    friend Matrix operator*(const T& d, const Matrix& m)
+    {
+        return m * d;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& m)
+    {
+        // TODO determine sane length based on string representation of all values
+        size_t length = 4;
+        for (size_t row = 0; row < m.m_rows; row++)
+        {
+            for (size_t col = 0; col < m.m_columns; col++)
+            {
+                os << std::setw(length) << m(row, col) << ", ";
+            }
+
+            os << std::endl;
+        }
+
+        return os;
+    }
+
+    T& operator()(size_t row, size_t col) { return m_data[pos(row, col)]; }
+    const T& operator()(size_t row, size_t col) const { return m_data[pos(row, col)]; }
+
+    T max() const
+    {
+        if (m_rows == 0 || m_columns == 0)
+        {
+            throw std::exception("Matrix was created without values (no rows or no columns)");
+        }
+        double max_found = m_data[0];
+        for (size_t pos = 0; pos < m_rows * m_columns; pos++)
+        {
+            max_found = std::max(max_found, m_data[pos]);
+        }
+        return max_found;
+    }
 
 private:
     size_t pos(size_t row, size_t column) const { return row * m_columns + column; }
 
 private:
-    double* m_data;
+    T* m_data;
     size_t m_rows;
     size_t m_columns;
 };
--- main.cpp
+++ main.cpp
@@ -5,22 +5,23 @@
 int main(int argc, char* argv)
 {
 	std::cout << "Starting Matrix test" << std::endl;
-    Matrix m1(2, 3);
+    Matrix<double> m1(2, 3);
 
     m1(0, 0) =  0; m1(0, 1) =  1; m1(0, 2) =  2;
     m1(1, 0) = 10; m1(1, 1) = 11; m1(1, 2) = 12;
 
-    Matrix m2(2, 3);
+    Matrix<double> m2(2, 3);
 
     m2(0, 0) =  100; m2(0, 1) =  200; m2(0, 2) =  300;
     m2(1, 0) = 1100; m2(1, 1) = 1200; m2(1, 2) = 1300;
 
     std::cout << "===\nm1:\n" << m1 << "===\nm2:\n" << m2 << std::endl;
 
-    Matrix m3 = m1 + m2;
-    Matrix m4 = 3 * m1;
-    Matrix m5 = m4;
+    Matrix<double> m3 = m1 + m2;
+    Matrix<double> m4 = 3 * m1;
+    Matrix<double> const &m5 = m4;
 
+    //forbidden: m5(1, 1) = 42;
     std::cout << "===\nm1+m2:\n" << m3 << "===\nm1*3:\n" << m5 << std::endl;
 
     m1 = m3;
